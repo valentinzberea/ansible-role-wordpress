0b26820994b446e8a4f07825fb1704f157fe6553
Valentin Zberea <valentin.zberea@gmail.com>
2016-04-18 12:12:34 +0300

    Remove secret keys from wp-config

--- templates/wp-config.php
+++ templates/wp-config.php
@@ -25,15 +25,6 @@ define('WP_HOME', $protocol_to_use . $_SERVER['HTTP_HOST']);
 // Multi Site
 define('WP_ALLOW_MULTISITE', {{ wp_allow_multisite }});
 
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
 $table_prefix  = 'wp_';
 
 define('WP_DEBUG', false);
